d9ed58353d7e261fb8e0182b58ba6f32f5a22abf
Dan S <drinkwithdan@gmail.com>
2022-09-08 10:05:27 +1000

    Daily coding 8.9

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -7,9 +7,7 @@ def capitalize_title(title):
     :param title: str - title string that needs title casing.
     :return: str - title string in title case (first letters capitalized).
     """
-
-    pass
-
+    return title.title()
 
 def check_sentence_ending(sentence):
     """Check the ending of the sentence to verify that a period is present.
@@ -17,9 +15,7 @@ def check_sentence_ending(sentence):
     :param sentence: str - a sentence to check.
     :return: bool - return True if punctuated correctly with period, False otherwise.
     """
-
-    pass
-
+    return sentence[-1] == "."
 
 def clean_up_spacing(sentence):
     """Verify that there isn't any whitespace at the start and end of the sentence.
@@ -27,9 +23,7 @@ def clean_up_spacing(sentence):
     :param sentence: str - a sentence to clean of leading and trailing space characters.
     :return: str - a sentence that has been cleaned of leading and trailing space characters.
     """
-
-    pass
-
+    return sentence.strip()
 
 def replace_word_choice(sentence, old_word, new_word):
     """Replace a word in the provided sentence with a new one.
@@ -39,5 +33,4 @@ def replace_word_choice(sentence, old_word, new_word):
     :param new_word: str - replacement word.
     :return: str - input sentence with new words in place of old words.
     """
-
-    pass
+    return sentence.replace(old_word, new_word)
